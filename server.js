6dade3967299fc6058ae338a46b9a1ac96c316ae
TRNisbet <TRNisbet@hotmail.com>
2021-02-22 14:17:18 -0500

    renamed the DB again

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(require('./routes/api-routes'));
 app.use(require('./routes/html-routes'));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/TRN-HW-17',
+    process.env.MONGODB_URI || 'mongodb://localhost/HW17',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
