4bf6edb91eee8299093821ecfad152d5e445005c
TRNisbet <TRNisbet@hotmail.com>
2021-02-22 12:42:35 -0500

    deploying

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.use(require('./routes/api-routes'));
+app.use(require('./routes/html-routes'));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workouts',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
+});
